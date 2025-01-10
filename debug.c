82f27cc38395677272d12407210439f207492802
bhqv <bhavi374@gmail.com>
2025-01-10 17:37:03 +0530

    disassemble line info

--- debug.c
+++ debug.c
@@ -4,6 +4,7 @@
 
 #include "debug.h"
 #include "chunk.h"
+#include "value.h"
 
 void disassembleChunk(Chunk *chunk, const char *name){
     printf("==%s==\n", name);
@@ -14,6 +15,17 @@ void disassembleChunk(Chunk *chunk, const char *name){
     }
 }
 
+static int constantInstruction(const char* name, Chunk* chunk,
+        int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
+
+}
+
+
 static int simpleInstruction(const char* name, int offset) {
     printf("%s\n", name);
     return offset + 1;
@@ -21,9 +33,18 @@ static int simpleInstruction(const char* name, int offset) {
 
 int disassembleInstruction(Chunk *chunk, int offset) {
     printf("%04d ", offset);
+    if (offset > 0 &&
+            chunk->lines[offset] == chunk->lines[offset - 1]) {
+        printf("   | ");
+    } else {
+        printf("%4d ", chunk->lines[offset]);
+    }
+
 
     uint8_t instrunction = chunk -> code[offset];
     switch (instrunction) {
+        case OP_CONSTANT:
+            return constantInstruction("OP_CONSTANT", chunk, offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
         default:
