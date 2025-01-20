991cef3e661cc7e38f23e7cd024f7e7571027157
bhqv <bhavi374@gmail.com>
2025-01-20 23:37:20 +0530

    comments

---
+++ compiler.h
@@ -0,0 +1,6 @@
+#ifndef coompiler_compiler_h
+#define coompiler_compiler_h
+
+void compile(const char* source);
+
+#endif
--- vm.c
+++ vm.c
@@ -2,6 +2,7 @@
 #include "chunk.h"
 #include "common.h"
 #include "vm.h"
+#include "compiler.h"
 #include "debug.h"
 
 VM vm;
@@ -74,9 +75,11 @@ static InterpretResult run() {
 }
 
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
+    // vm.chunk = chunk;
+    // vm.ip = vm.chunk->code;
+    // return run();
 }
 
