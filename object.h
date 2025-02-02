91c63ae2bff38f4f88c30e3448fe6efec72bad64
bhqv <bhavi374@gmail.com>
2025-02-03 00:26:06 +0530

    Strin started

---
+++ object.h
@@ -0,0 +1,17 @@
+#ifndef coom_object_h
+#define coom_object_h
+
+#include "common.h"
+#include "value.h"
+
+#define OBJ_TYPE(value) (AS_OBJ(value)->type)
+
+typedef enum {
+    OBJ_STRING
+} ObjType;
+
+struct Obj {
+    ObjType type;
+};
+
+#endif
