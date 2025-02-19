c6d09dfc50a95f5efd2c8f8afc616a27e435e01a
bhqv <bhavi374@gmail.com>
2025-02-20 00:55:07 +0530

    allocatin and resize

---
+++ table.h
@@ -0,0 +1,24 @@
+#ifndef coom_table_h
+#define coom_table_h
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+    ObjString* key;
+    Value value;
+} Entry;
+
+typedef struct {
+    int count;
+    int capacity;
+    Entry* entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableSet(Table* table, ObjString* key, Value value);
+void tableAddAll(Table* from, Table* to);
+
+
+#endif
